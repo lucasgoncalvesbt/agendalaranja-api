7c5d1967831c71c27d9b0e66393aaea39cf60303
Lucas Gonçalves <front.end.lb@gmail.com>
2021-09-02 23:32:06 -0300

    feat: adicionando arquivos iniciais do projeto

---
+++ src/main/java/br/com/fcamara/hackatonapi/HackatonApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fcamara.hackatonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fcamara/hackatonapi/HackatonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fcamara.hackatonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
