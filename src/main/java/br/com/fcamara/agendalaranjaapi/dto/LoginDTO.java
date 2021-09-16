070a3e95d4b5c908cb67f2ac4368b801136022c5
Lucas Gonçalves <front.end.lb@gmail.com>
2021-09-16 17:04:23 -0300

    fix: arrumando os dto colocando os construtores

--- src/main/java/br/com/fcamara/agendalaranjaapi/dto/LoginDTO.java
+++ src/main/java/br/com/fcamara/agendalaranjaapi/dto/LoginDTO.java
@@ -1,10 +1,14 @@
 package br.com.fcamara.agendalaranjaapi.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginDTO {
 
     private String email;
